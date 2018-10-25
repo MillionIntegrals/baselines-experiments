bb57877326574c768f0f777ea5c74878abdc615f
Million Integrals <jerry@millionintegrals.com>
2018-10-25 21:44:25 +0200

    Local mujoco scripts.

---
+++ experiments/scripts/local-run-mujoco-ppo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+vel mujoco/ppo/half_cheetah_ppo.yaml train -r 7 --seed 7
+vel mujoco/ppo/half_cheetah_ppo.yaml train -r 8 --seed 8
+vel mujoco/ppo/half_cheetah_ppo.yaml train -r 9 --seed 9
+vel mujoco/ppo/half_cheetah_ppo.yaml train -r 10 --seed 10
+vel mujoco/ppo/half_cheetah_ppo.yaml train -r 11 --seed 11
+vel mujoco/ppo/half_cheetah_ppo.yaml train -r 12 --seed 12
+
+vel mujoco/ppo/hopper_ppo.yaml train -r 7 --seed 7
+vel mujoco/ppo/hopper_ppo.yaml train -r 8 --seed 8
+vel mujoco/ppo/hopper_ppo.yaml train -r 9 --seed 9
+vel mujoco/ppo/hopper_ppo.yaml train -r 10 --seed 10
+vel mujoco/ppo/hopper_ppo.yaml train -r 11 --seed 11
+vel mujoco/ppo/hopper_ppo.yaml train -r 12 --seed 12
+
+vel mujoco/ppo/inverted_double_pendulum_ppo.yaml train -r 7 --seed 7
+vel mujoco/ppo/inverted_double_pendulum_ppo.yaml train -r 8 --seed 8
+vel mujoco/ppo/inverted_double_pendulum_ppo.yaml train -r 9 --seed 9
+vel mujoco/ppo/inverted_double_pendulum_ppo.yaml train -r 10 --seed 10
+vel mujoco/ppo/inverted_double_pendulum_ppo.yaml train -r 11 --seed 11
+vel mujoco/ppo/inverted_double_pendulum_ppo.yaml train -r 12 --seed 12
+
+vel mujoco/ppo/inverted_pendulum_ppo.yaml train -r 7 --seed 7
+vel mujoco/ppo/inverted_pendulum_ppo.yaml train -r 8 --seed 8
+vel mujoco/ppo/inverted_pendulum_ppo.yaml train -r 9 --seed 9
+vel mujoco/ppo/inverted_pendulum_ppo.yaml train -r 10 --seed 10
+vel mujoco/ppo/inverted_pendulum_ppo.yaml train -r 11 --seed 11
+vel mujoco/ppo/inverted_pendulum_ppo.yaml train -r 12 --seed 12
+
+vel mujoco/ppo/reacher_ppo.yaml train -r 7 --seed 7
+vel mujoco/ppo/reacher_ppo.yaml train -r 8 --seed 8
+vel mujoco/ppo/reacher_ppo.yaml train -r 9 --seed 9
+vel mujoco/ppo/reacher_ppo.yaml train -r 10 --seed 10
+vel mujoco/ppo/reacher_ppo.yaml train -r 11 --seed 11
+vel mujoco/ppo/reacher_ppo.yaml train -r 12 --seed 12
+
+vel mujoco/ppo/swimmer_ppo.yaml train -r 7 --seed 7
+vel mujoco/ppo/swimmer_ppo.yaml train -r 8 --seed 8
+vel mujoco/ppo/swimmer_ppo.yaml train -r 9 --seed 9
+vel mujoco/ppo/swimmer_ppo.yaml train -r 10 --seed 10
+vel mujoco/ppo/swimmer_ppo.yaml train -r 11 --seed 11
+vel mujoco/ppo/swimmer_ppo.yaml train -r 12 --seed 12
+
+vel mujoco/ppo/walker2d_ppo.yaml train -r 7 --seed 7
+vel mujoco/ppo/walker2d_ppo.yaml train -r 8 --seed 8
+vel mujoco/ppo/walker2d_ppo.yaml train -r 9 --seed 9
+vel mujoco/ppo/walker2d_ppo.yaml train -r 10 --seed 10
+vel mujoco/ppo/walker2d_ppo.yaml train -r 11 --seed 11
+vel mujoco/ppo/walker2d_ppo.yaml train -r 12 --seed 12
---
+++ experiments/scripts/local-run-mujoco-trpo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+vel mujoco/trpo/half_cheetah_trpo.yaml train -r 7 --seed 7
+vel mujoco/trpo/half_cheetah_trpo.yaml train -r 8 --seed 8
+vel mujoco/trpo/half_cheetah_trpo.yaml train -r 9 --seed 9
+vel mujoco/trpo/half_cheetah_trpo.yaml train -r 10 --seed 10
+vel mujoco/trpo/half_cheetah_trpo.yaml train -r 11 --seed 11
+vel mujoco/trpo/half_cheetah_trpo.yaml train -r 12 --seed 12
+
+vel mujoco/trpo/hopper_trpo.yaml train -r 7 --seed 7
+vel mujoco/trpo/hopper_trpo.yaml train -r 8 --seed 8
+vel mujoco/trpo/hopper_trpo.yaml train -r 9 --seed 9
+vel mujoco/trpo/hopper_trpo.yaml train -r 10 --seed 10
+vel mujoco/trpo/hopper_trpo.yaml train -r 11 --seed 11
+vel mujoco/trpo/hopper_trpo.yaml train -r 12 --seed 12
+
+vel mujoco/trpo/inverted_double_pendulum_trpo.yaml train -r 7 --seed 7
+vel mujoco/trpo/inverted_double_pendulum_trpo.yaml train -r 8 --seed 8
+vel mujoco/trpo/inverted_double_pendulum_trpo.yaml train -r 9 --seed 9
+vel mujoco/trpo/inverted_double_pendulum_trpo.yaml train -r 10 --seed 10
+vel mujoco/trpo/inverted_double_pendulum_trpo.yaml train -r 11 --seed 11
+vel mujoco/trpo/inverted_double_pendulum_trpo.yaml train -r 12 --seed 12
+
+vel mujoco/trpo/inverted_pendulum_trpo.yaml train -r 7 --seed 7
+vel mujoco/trpo/inverted_pendulum_trpo.yaml train -r 8 --seed 8
+vel mujoco/trpo/inverted_pendulum_trpo.yaml train -r 9 --seed 9
+vel mujoco/trpo/inverted_pendulum_trpo.yaml train -r 10 --seed 10
+vel mujoco/trpo/inverted_pendulum_trpo.yaml train -r 11 --seed 11
+vel mujoco/trpo/inverted_pendulum_trpo.yaml train -r 12 --seed 12
+
+vel mujoco/trpo/reacher_trpo.yaml train -r 7 --seed 7
+vel mujoco/trpo/reacher_trpo.yaml train -r 8 --seed 8
+vel mujoco/trpo/reacher_trpo.yaml train -r 9 --seed 9
+vel mujoco/trpo/reacher_trpo.yaml train -r 10 --seed 10
+vel mujoco/trpo/reacher_trpo.yaml train -r 11 --seed 11
+vel mujoco/trpo/reacher_trpo.yaml train -r 12 --seed 12
+
+vel mujoco/trpo/swimmer_trpo.yaml train -r 7 --seed 7
+vel mujoco/trpo/swimmer_trpo.yaml train -r 8 --seed 8
+vel mujoco/trpo/swimmer_trpo.yaml train -r 9 --seed 9
+vel mujoco/trpo/swimmer_trpo.yaml train -r 10 --seed 10
+vel mujoco/trpo/swimmer_trpo.yaml train -r 11 --seed 11
+vel mujoco/trpo/swimmer_trpo.yaml train -r 12 --seed 12
+
+vel mujoco/trpo/walker2d_trpo.yaml train -r 7 --seed 7
+vel mujoco/trpo/walker2d_trpo.yaml train -r 8 --seed 8
+vel mujoco/trpo/walker2d_trpo.yaml train -r 9 --seed 9
+vel mujoco/trpo/walker2d_trpo.yaml train -r 10 --seed 10
+vel mujoco/trpo/walker2d_trpo.yaml train -r 11 --seed 11
+vel mujoco/trpo/walker2d_trpo.yaml train -r 12 --seed 12
